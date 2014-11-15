bfa8655e48b451c05fe6c6c877da843fda70ce3b
Dave Seidman <bullcitydave@gmail.com>
2014-11-15 18:35:07 -0500

    Deploy to gh-pages

--- gulpfile.js
+++ gulpfile.js
@@ -147,3 +147,23 @@ gulp.task('watch', ['connect', 'serve'], function () {
     gulp.watch('app/images/**/*', ['images']);
     gulp.watch('bower.json', ['wiredep']);
 });
+
+// Push a subtree from our `dist` folder
+gulp.task('deploy', function() {
+
+  gulp.src('/')
+    .pipe(prompt.prompt({
+        type: 'confirm',
+        name: 'task',
+        message: 'This will deploy to GitHub Pages. Have you already built your application and pushed your updated master branch?'
+    }, function(res){
+      if (res.task){
+        console.log('Attempting: "git subtree push --prefix dist origin gh-pages"');
+        exec('git subtree push --prefix dist origin gh-pages', function(err, stdout, stderr) {
+            console.log(stdout);
+            console.log(stderr);
+        });
+      } else { console.log('Please do this first and then run `gulp deploy` again.'); }
+    }));
+
+});
